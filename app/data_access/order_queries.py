1298b8a8fad43e9d8619442bec9ccca28672d102
Dhruva-123 <rahuldhruva.k9@gmail.com>
2026-08-31 14:34:11 +0530

    @

--- app/data_access/order_queries.py
+++ app/data_access/order_queries.py
@@ -1,13 +1,10 @@
 from sqlalchemy import text
-
 from app.database import engine
-
 
 def get_stock_id_by_symbol(symbol: str):
     with engine.connect() as connection:
         query = text("SELECT id FROM Stocks WHERE symbol = :symbol")
         return connection.execute(query, {"symbol": symbol}).scalar_one_or_none()
-
 
 def create_buy_order(user_id: str, stock_id: int, quantity: int, price: float):
     with engine.connect() as connection:
@@ -25,7 +22,6 @@ def create_buy_order(user_id: str, stock_id: int, quantity: int, price: float):
             },
         )
         connection.commit()
-
 
 def create_sell_order(user_id: str, stock_id: int, quantity: int, price: float):
     with engine.connect() as connection:
